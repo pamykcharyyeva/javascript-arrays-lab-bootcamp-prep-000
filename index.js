45f536fe99d85a9be77a8264e303e202c0be03ae
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-01-14 20:37:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ var kittens = ['Milo','Otis','Garfield'] //define your array here
 function destructivelyAppendKitten(name){
  kittens.push(name)
  return kittens
-}
+};
+function destructivelyPretendKitten(name){
+ kittens.push(name)
+ return kittens
+};
