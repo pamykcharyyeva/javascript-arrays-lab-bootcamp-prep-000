f45f1600a249517825e3756c01ea76fdd2463b0e
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-01-14 21:29:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 var kittens = ['Milo','Otis','Garfield'] //define your array here
 
-function destructivelyAppendKitten(name){
- kittens.push(name)
- return kittens
-};
-function destructivelyPrependKitten(name){
- kittens.unshift(name)
- return kittens;
-};
-function destructivelyRemoveLastKitten (name){
-  kitten.shift();
-  return kitten;
-};
-function appendKitten(name){
-  var newArray = kittens.slice();
-  newArray.push(name)
-  return newArray
-};
-function prependKitten(name) {
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+ function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+ function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+ function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+ function appendKitten(name){
+  return [...kittens, name];
+}
+
+ function prependKitten(name) {
   return [name, ...kittens];
-};
+}
+
+ function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+ function removeFirstKitten() {
+  return kittens.slice(1);
+}
