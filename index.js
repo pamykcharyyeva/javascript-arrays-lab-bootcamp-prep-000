d709b2ffd5c33547007d70121308dbc9bb877482
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-01-14 20:48:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyPrependKitten(name){
  kittens.unshift(name)
  return kittens;
 };
-function destructivelyRemoveLastKitten (){
+function destructivelyRemoveLastKitten (kitten){
   kitten.shift();
   return kitten;
 };
