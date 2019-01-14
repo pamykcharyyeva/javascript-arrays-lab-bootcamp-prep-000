effb4c2ae35ce559748c50dcff5584f501f2bc03
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-01-14 20:44:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function destructivelyAppendKitten(name){
  kittens.push(name)
  return kittens
 };
-function ddestructivelyPrependKitten(name){
+function destructivelyPrependKitten(name){
  kittens.unshift(name)
  return kittens;
 };
